42e1613920d7210865689678582e716f50555670
Fredy <jfr3dy90@gmail.com>
2022-05-31 14:06:58 -0500

    Start Pick Items

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class PlayerInteractions : MonoBehaviour
 {
     public Transform cameraPlayer;
+    public Transform objetoVacio;
+    public LayerMask lm;
 
     private void Update()
     {
-        Debug.DrawRay(cameraPlayer.position, cameraPlayer.forward, Color.green);       
+        Debug.DrawRay(cameraPlayer.position, cameraPlayer.forward, Color.green);
+
+        RaycastHit hit;
+        if (Physics.Raycast(cameraPlayer.position, cameraPlayer.forward, out hit, 2f, lm))
+        { 
+            if (Input.GetButtonDown("PickButton"))
+            {
+                hit.transform.parent = objetoVacio;
+                hit.transform.localPosition = Vector3.zero;
+                Debug.Log(hit.transform.name);
+            }
+        }
     }
 
 
